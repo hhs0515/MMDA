34b100d4bfc0909cd113cddda2d4593281d519c9
hhs0515 <hhs0515@postech.ac.kr>
2023-11-17 18:30:58 +0900

    main content + app bar design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.csed404.mmda"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.csed404.mmda"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
