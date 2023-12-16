d13bb299eb1b72ef0d512ac7c210f7c20314a187
hhs0515 <hhs0515@postech.ac.kr>
2023-12-17 01:27:29 +0900

    Import dependency and permission for Google calendar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,4 +43,13 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation ("com.google.android.gms:play-services-auth:20.7.0")
+    implementation ("pub.devrel:easypermissions:0.3.0")
+    implementation("com.google.api-client:google-api-client-android:1.22.0") {
+        exclude("org.apache.httpcomponents")
+    }
+    implementation("com.google.apis:google-api-services-calendar:v3-rev235-1.22.0") {
+        exclude("org.apache.httpcomponents")
+    }
 }
